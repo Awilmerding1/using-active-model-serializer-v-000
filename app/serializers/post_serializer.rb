f4f4ec8bb166ae2b5efd11311d100029eb36e682
Annabel Wilmerding <annabelwilmerding@gmail.com>
2018-10-11 19:43:11 +0000

    automatically backed up by learn

--- app/serializers/post_serializer.rb
+++ app/serializers/post_serializer.rb
@@ -1,4 +1,8 @@
 class PostSerializer < ActiveModel::Serializer
   attributes :id, :title, :description
+<<<<<<< HEAD
   belongs_to :author, serializer: PostAuthorSerializer
+=======
+  belongs_to :author
+>>>>>>> b7d67a20b4d30a4b9a16f2f9bf634fefd1070f9c
 end
